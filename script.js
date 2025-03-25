a7e6aaf38945233f9adae294855974b4f22c4121
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-25 17:50:22 +0100

    [FIX] Riordinato il codice e aggiunti commenti

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+// Richiamo gli elementi del DOM 
 const spenta = document.querySelector('.spenta')
 
 const accesa = document.querySelector('.accesa')
 
 const button = document.getElementById('button')
 
-button.addEventListener('click' , function(){
-    console.log('CLICCATO')
-    //spenta.classList.add('.')
-    accesa.classList.remove('.d-none');
+let isAccesa = false;
+
+// scrivo la funzione che si verifica al click del bottone
+button.addEventListener('click', function () {
+    console.log('Hai cliccato il bottone', isAccesa)
+    // se è spenta, accendila
+    if (isAccesa == false) {
+        spenta.classList.add('d-none')
+        accesa.classList.remove('d-none');
+        isAccesa = true;
+
+    }
+    // se è accesa, spegnila
+    else {
+        spenta.classList.remove('d-none')
+        accesa.classList.add('d-none');
+        isAccesa = false;
+    }
 })
